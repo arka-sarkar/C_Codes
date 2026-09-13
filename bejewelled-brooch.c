0e3a4d17aed3132de45d2f74d384d9c282b58c84
Arka Sarkar <arkasarkar389@gmail.com>
2026-09-13 17:44:28 +0530

    small change in bejewelled-brooch.c

--- bejewelled-brooch.c
+++ bejewelled-brooch.c
@@ -3,10 +3,8 @@
 int main(int argc, char* argv[]) {
     int m,n;
     scanf("%d %d", &m, &n);
-    for (int i = 1; i <= 2*m+1; i++)
-    {
-        for (int j = 1; j <= 2*n+1; j++)
-        {
+    for (int i = 1; i <= 2*m+1; i++) {
+        for (int j = 1; j <= 2*n+1; j++) {
             if (i%2) putchar('0');
             else {
                 if (j%2) putchar('0');
