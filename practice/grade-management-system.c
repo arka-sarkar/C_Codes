2aa74ec139187f02b3d32e4d88746271b0a20317
Arka Sarkar <arkasarkar389@gmail.com>
2026-09-13 21:09:12 +0530

    wrote a new code: student grade management system

---
+++ practice/grade-management-system.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[]) {
+    int max=0, min=20, choice, mark, pos=0, sum=0, pos1=0;
+    float avg;
+    int marks[30]={0};
+    int rollno[30]={0};
+    while (1) {
+        printf("\n===== STUDENT MARKS =====\n");
+        printf("1. Enter Marks\n");
+        printf("2. Display Marks\n");
+        printf("3. Calculate Average\n");
+        printf("4. Find Highest Mark\n");
+        printf("5. Find Lowest Mark\n");
+        printf("6. Search for a Mark\n");
+        printf("7. Exit\n");
+        
+        printf("\nEnter the corresponding number for the choice: ");
+        scanf(" %d", &choice);
+
+        switch (choice) {
+            case 1:
+                if(pos < 30) scanf(" %d", &marks[pos++]);
+                else printf("Your have already entered the marks of all students.\n");
+                break;
+            
+            case 2:
+                for(int i=0; i<pos; i++) {
+                    if( i != 0 ) putchar(' ');
+                    printf("%d", marks[i]);
+                }
+                break;
+            
+            case 3:
+                for(int i=0; i<pos; i++) sum += marks[i];
+                avg = (float)sum/pos;
+                printf("The average mark is %.1f.\n", avg);
+                sum = 0;
+                break;
+
+            case 4:
+                for(int i=0; i<pos; i++) {
+                    if(max < marks[i]) max == marks[i];
+                }
+                printf("The maximum marks is %d.\n", max);
+                break;
+            
+            case 5:
+                for(int i=0; i<pos; i++) {
+                    if(min > marks[i]) min == marks[i];
+                }
+                printf("The maximum marks is %d.\n", min);
+                break;
+            
+            case 6:
+                scanf(" %d", &mark);
+
+                for(int i=0; i<pos; i++) {
+                    if(mark == marks[i]) rollno[pos1++] = pos+1;
+                }
+                if(!rollno[0]){
+                    printf("Students with roll no. ");
+                    for(int i=0; i<pos1; i++) {
+                        printf("%d", rollno[pos1]);
+                        if(i != pos-1) printf(", ");
+                    }
+                    putchar('\n');
+                } else printf("No students got %d marks.\n", mark);
+                break;
+
+            case 7:
+                return 0;
+
+            default:
+                printf("You entered the wrong option.\n");
+                break;
+        }
+    }
+    
+    return 0;
+}
