8dbca1f7285618b7b155c461b03176989244c5ab
Arka Sarkar <arkasarkar389@gmail.com>
2026-09-16 12:54:01 +0530

    corrected array.c

--- arrays/array.c
+++ arrays/array.c
@@ -4,9 +4,9 @@
 int main(int argc, char* argv[]) {
     int* a=calloc(5,sizeof(int));
     for (int i = 0; i < 5; i++) scanf(" %d",a+i);
-    for (int i = 0; i < 5; i++) {if(!i) printf(" "); printf("%p",(a+i));}
+    for (int i = 0; i < 5; i++) {if(i) printf(" "); printf("%p",(a+i));}
     putchar('\n');
-    for (int i = 0; i < 5; i++) {if(!i) printf(" "); printf("%d",*(a+i));}
+    for (int i = 0; i < 5; i++) {if(i) printf(" "); printf("%d",*(a+i));}
     putchar('\n');
     free(a);
     a=NULL;
